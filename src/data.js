e41f70c5a9512a8d9710d2ddc3db206c47eab931
Nurlan <nurlanimamali@gmail.com>
2023-04-28 20:32:58 +0200

    last changes

--- src/data.js
+++ src/data.js
@@ -14,30 +14,6 @@ export const quizData = {
   // the questions in the quiz
   questions: [
     {
-      text: 'Which is the largest continent in the world?' ,
-      answers: {
-        a: 'Asia',
-        b: 'Africa',
-        c: 'North America',
-        d: 'Europe'
-      },
-      correct: 'a',
-      selected: null,
-      info: "Asia is the largest continent in the world, covering approximately 30% of the Earth's land area. It is home to over 4.5 billion people and contains a diverse range of cultures, languages, and religions."
-    },
-    {
-      text: 'What is the capital of Australia?',
-      answers: {
-        a: 'Sydney',
-        b: 'Melbourne',
-        c: 'Canberra',
-        d: 'Brisbane',
-      },
-      correct: 'c',
-      selected: null,
-      info: 'Canberra is the capital city of Australia. It is located in the Australian Capital Territory and is the seat of the federal government. The city has a population of around 400,000 people and is known for its modern architecture and beautiful parks.'
-    },
-    {
       text: 'How many continents are there in the world?',
       answers: {
         a: '4',
@@ -47,8 +23,9 @@ export const quizData = {
       },
       correct: 'd',
       selected: null,
-      info: 'There are seven continents in the world: Asia, Africa, North America, South America, Antarctica, Europe, and Australia. Each continent has its own unique features, such as geography, climate, and culture.'
-    },{
+      info: 'There are seven continents in the world: Asia, Africa, North America, South America, Antarctica, Europe, and Australia. Each continent has its own unique features, such as geography, climate, and culture.',
+    },
+    {
       text: 'What is the longest river in the world?',
       answers: {
         a: 'Nile',
@@ -58,8 +35,9 @@ export const quizData = {
       },
       correct: 'a',
       selected: null,
-      info: 'The Nile River is the longest river in the world, stretching over 4,100 miles through 11 African countries. It is considered to be the lifeline of Egypt and has played a crucial role in the development of civilization in the region.'
-    },{
+      info: 'The Nile River is the longest river in the world, stretching over 4,100 miles through 11 African countries. It is considered to be the lifeline of Egypt and has played a crucial role in the development of civilization in the region.',
+    },
+    {
       text: 'What is the largest world religion?',
       answers: {
         a: 'Christianity',
@@ -70,7 +48,8 @@ export const quizData = {
       correct: 'a',
       selected: null,
       info: 'Christianity is the largest world religion, with approximately 2.4 billion followers worldwide. It is a monotheistic religion based on the teachings of Jesus Christ and has had a significant impact on world history and culture.',
-    },{
+    },
+    {
       text: 'What is the smallest country in the world with an area of only 0.49 square kilometers?',
       answers: {
         a: 'Monaco',
@@ -80,7 +59,7 @@ export const quizData = {
       },
       correct: 'c',
       selected: null,
-      info: 'The smallest country in the world is Vatican City, with a landmass of just 0.49 square kilometers (0.19 square miles). Vatican City is an independent state surrounded by Rome. Vatican City is not the only small country located inside Italy.'
+      info: 'The smallest country in the world is Vatican City, with a landmass of just 0.49 square kilometers (0.19 square miles). Vatican City is an independent state surrounded by Rome. Vatican City is not the only small country located inside Italy.',
     },
     {
       text: 'What country does this flag belong to?',
@@ -91,32 +70,11 @@ export const quizData = {
         d: 'Armenia',
       },
       correct: 'b',
-      image: "./src/images/azerbaijan.png",
+      image: './src/images/azerbaijan.png',
       selected: null,
-      info: "The flag has become the predominant and most recognizable symbol of Azerbaijan. The bright blue represents Azerbaijan's Turkic heritage, the red represents progress, and the green represents Islam, which is Azerbaijan's majority religion."
-    },{
-      text: 'Which of the following is a landlocked country, meaning it is not bordered by the sea or ocean?',
-      answers: {
-        a: 'Kazakhstan',
-        b: 'Madagascar',
-        c: 'Turkey',
-        d: 'Russia',
-      },
-      correct: 'a',
-      selected: null,
-      info: 'Countries such as Kazakhstan, in Central Asia, that only have access to a landlocked sea such as the Caspian are considered landlocked.'
-    },{
-      text: 'Which of the following countries is often regarded as the oldest republic in the world?',
-      answers: {
-        a: 'Spain',
-        b: 'Greece',
-        c: 'Italy',
-        d: 'San Marino',
-      },
-      correct: 'd',
-      selected: null,
-      info: 'San Marino claims to be the oldest constitutional republic in the world, founded on 3 September 301, by Marinus of Rab, a Christian stonemason fleeing the religious persecution of Roman Emperor Diocletian.'
-    },{
+      info: "The bright blue represents Azerbaijan's Turkic heritage, the red represents progress, and the green represents Islam, which is Azerbaijan's majority religion.",
+    },
+    {
       text: 'Who is the current king of the Netherlands?',
       answers: {
         a: 'Michael Jackson',
@@ -126,8 +84,9 @@ export const quizData = {
       },
       correct: 'd',
       selected: null,
-      info: 'On 30 April 2013, King Willem-Alexander succeeded his mother as monarch. The King is married to Queen Máxima. The royal couple have three daughters, Princess Catharina-Amalia (the Princess of Orange), Princess Alexia and Princess Ariane.'
-    },{
+      info: 'On 30 April 2013, King Willem-Alexander succeeded his mother as monarch. The King is married to Queen Máxima. The royal couple have three daughters, Princess Catharina-Amalia (the Princess of Orange), Princess Alexia and Princess Ariane.',
+    },
+    {
       text: 'Which country turned carrots orange?',
       answers: {
         a: 'England',
@@ -136,22 +95,11 @@ export const quizData = {
         d: 'Egypt',
       },
       correct: 'b',
-      image: "./src/images/carrot.png",
+      image: './src/images/carrot.png',
       selected: null,
-      info: 'The Netherlands is the country that turned carrots orange. In the 17th century, Dutch growers cultivated orange carrots as a tribute to William of Orange who led the struggle for Dutch independence.'
-    },{
-      text: 'What is the dish made of Rocky Mountain oysters?',
-      answers: {
-        a: 'Oyster',
-        b: 'Fish',
-        c: 'Meat balls',
-        d: 'Cheese',
-      },
-      correct: 'c',
-      image: "./src/images/oyster.png",
-      selected: null,
-      info: 'Rocky Mountain oysters, also known as mountain oysters or prairie oysters in Canada (French: animelles), is a dish made of bull testicles. The organs are often deep-fried after being skinned, coated in flour, pepper and salt, and sometimes pounded flat. The dish is most often served as an appetizer.'
-    },{
+      info: 'The Netherlands is the country that turned carrots orange. In the 17th century, Dutch growers cultivated orange carrots as a tribute to William of Orange who led the struggle for Dutch independence.',
+    },
+    {
       text: 'Which continent is one of the driest continents on Earth?',
       answers: {
         a: 'Africa',
@@ -161,8 +109,9 @@ export const quizData = {
       },
       correct: 'c',
       selected: null,
-      info: 'Antarctica is one of the driest continents on Earth. The average annual rainfall at the South Pole over the past 30 years was just over 10 mm (0.4 in), classifying it as a polar desert.'
-    },{
+      info: 'Antarctica is one of the driest continents on Earth. The average annual rainfall at the South Pole over the past 30 years was just over 10 mm (0.4 in), classifying it as a polar desert.',
+    },
+    {
       text: 'What is the biggest fish in the ocean?',
       answers: {
         a: 'Whale shark',
@@ -171,21 +120,22 @@ export const quizData = {
         d: 'Killer whale',
       },
       correct: 'a',
-      image: "./src/images/whale.png",
+      image: './src/images/whale.png',
       selected: null,
-      info: 'The biggest fish in the ocean is the whale shark, also known as Rhincodon typus. Despite their tremendous size and intimidating appearance, whale sharks are commonly docile and approachable.'
-    },{
+      info: 'The biggest fish in the ocean is the whale shark, also known as Rhincodon typus. Despite their tremendous size and intimidating appearance, whale sharks are commonly docile and approachable.',
+    },
+    {
       text: 'What country does this flag belong to?',
       answers: {
         a: 'Bermuda Islands',
-        b: 'South Georgia and South Sandwich Islands',
+        b: 'S.Georgia and S.S.Islands',
         c: 'State of Hawaii',
-        d: "The Cayman Islands",
+        d: 'The Cayman Islands',
       },
       correct: 'c',
-      image: "./src/images/hawaii.png",
+      image: './src/images/hawaii.png',
       selected: null,
-      info: "The flag of Hawaii (Hawaiian: Ka Hae Hawaiʻi) is the only U.S. state flag to include a foreign country's national flag. It has been used by the kingdom, protectorate, republic, and territory of Hawaii in addition to the current state design."
-    }
+      info: "The flag of Hawaii is the only U.S. state flag to include a foreign country's national flag. It's been used by the kingdom, protectorate, republic, and territory of Hawaii in addition to the current state design.",
+    },
   ],
 };
--- src/pages/finalPage.js
+++ src/pages/finalPage.js
@@ -5,62 +5,61 @@ import { initQuestionPage } from './questionPage.js';
 
 export const showFinalPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = "";
+  userInterface.innerHTML = '';
 
   const userName = getUserName();
 
   const totalPoints = 100;
 
   const finalMessage = document.createElement('div');
-  finalMessage.classList.add("final-page-div")
+  finalMessage.classList.add('final-page-div');
 
   // different text depending on a score
   if (quizData.score < 30) {
     finalMessage.innerText = `Better luck next time, ${userName}. You earned ${quizData.score} points out of ${totalPoints} possible`;
-    const gif = document.createElement("img")
-    gif.classList.add("gif-style")
-    gif.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2QwNmMyMzEwNjI0MDIwNzA3YmFhY2MxM2EyODcxNjZhNmFiZjJmYSZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/lYiQWU9xMJrBsb2RHX/giphy.gif"
-    finalMessage.appendChild(gif)
+    const gif = document.createElement('img');
+    gif.classList.add('gif-style');
+    gif.src = '../src/images/notgood.png';
+    finalMessage.appendChild(gif);
   } else if (quizData.score >= 30 && quizData.score <= 70) {
     finalMessage.innerText = `Good job, ${userName}! You earned ${quizData.score} points out of ${totalPoints} possible points.`;
-    const gif = document.createElement("img")
-    gif.classList.add("gif-style")
-    gif.src = "https://media1.giphy.com/media/20Mmcse81nQ38dSwvo/giphy.gif?cid=ecf05e47w8uow8d5kadl5u104fr07cigo7cm6h0292xced1y&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-    finalMessage.appendChild(gif)
+    const gif = document.createElement('img');
+    gif.classList.add('gif-style');
+    gif.src = '../src/images/good.png';
+    finalMessage.appendChild(gif);
   } else {
     finalMessage.innerText = `Well done, ${userName}! You earned ${quizData.score} points out of ${totalPoints} possible points.`;
-    const gif = document.createElement("img")
-    gif.classList.add("gif-style")
-    gif.src = "https://media0.giphy.com/media/2A760H1p8R9UNpYCba/giphy.gif?cid=ecf05e47jn0fff7tt2bxu3ludfytcix7fz5tf5ygd90awtfn&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-    finalMessage.appendChild(gif)
+    const gif = document.createElement('img');
+    gif.classList.add('gif-style');
+    gif.src = '../src/images/verygood.png';
+    finalMessage.appendChild(gif);
   }
 
   // New Game button
   const newGameButton = document.createElement('button');
-  newGameButton.classList.add("newgame-button-style")
+  newGameButton.classList.add('newgame-button-style');
   newGameButton.innerText = 'New Game';
   newGameButton.addEventListener('click', startNewGame);
   newGameButton.classList.add('button-style');
 
-
   userInterface.appendChild(finalMessage);
-  userInterface.appendChild(newGameButton)
+  userInterface.appendChild(newGameButton);
 
-  const superMarioTeam = document.createElement("div")
-  superMarioTeam.innerHTML = "Created with love by SuperMarioTeam "
-  superMarioTeam.classList.add("moving-text")
+  const superMarioTeam = document.createElement('div');
+  superMarioTeam.innerHTML = 'Created with love by SuperMarioTeam ';
+  superMarioTeam.classList.add('moving-text');
 
-  const avatar1 = document.createElement("img")
-  avatar1.src = ".\src\images\nurlan.gif"
-  avatar1.classList.add("avatar");
-  const avatar2 = document.createElement("img")
-  avatar2.src = ".\src\images\anastasia.gif"
-  avatar2.classList.add("avatar");
-  const avatar3 = document.createElement("img")
-  avatar3.src = ".\src\images\evghen.gif"
-  avatar3.classList.add("avatar");
+  const avatar1 = document.createElement('img');
+  avatar1.src = './src/images/nurlan.gif';
+  avatar1.classList.add('avatar');
+  const avatar2 = document.createElement('img');
+  avatar2.src = './src/images/anastasia.gif';
+  avatar2.classList.add('avatar');
+  const avatar3 = document.createElement('img');
+  avatar3.src = './src/images/evghen.gif';
+  avatar3.classList.add('avatar');
 
-  userInterface.appendChild(superMarioTeam)
+  userInterface.appendChild(superMarioTeam);
   superMarioTeam.appendChild(avatar1);
   superMarioTeam.appendChild(avatar2);
   superMarioTeam.appendChild(avatar3);
